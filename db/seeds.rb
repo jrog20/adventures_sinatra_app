f5b080d9596228a008d907e2dbc154080aa56d88
jrog20 <jaimerogstad@gmail.com>
2020-11-01 15:28:35 -0500

    create seeds

---
+++ db/seeds.rb
@@ -0,0 +1,6 @@
+jaime = User.create(username: "jrog", email: "jaimerogstad@gmail.com", password: "pw", full_name: "Jaime Rogstad", preferred_name: "J", image_url: "https://goputney.com/wp-content/uploads/2018/10/Jaime-Web.jpg", bio: "Born in Germany and raised in southern Vermont, Jaime joined Putney Student Travel 12 years ago. She is a member of Putney’s Travel Coordination team, coordinates group flights, and supports the Putney Open Door Fund scholarship foundation. Jaime has worked with our Pre-College program at Amherst College. She spends her free time with her family, horseback riding, and gardening.")
+rebekah = User.create()
+
+Adventure.create(image_url: "https://www.facebook.com/photo.php?fbid=618057066799&set=t.1517974235&type=3", city: "Liberia", country: "Costa Rica", date: "March, 2012")
+
+Adventure.create(image_url: "https://www.facebook.com/putneystudenttravel/photos/t.1517974235/10151626263045869/?type=3", city: "Avene", country: "France", date: "September, 2013")
