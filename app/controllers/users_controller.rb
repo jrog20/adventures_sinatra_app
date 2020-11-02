1abf87458a953a5a4e19cd7fc54529e57f1900b4
jrog20 <jaimerogstad@gmail.com>
2020-11-02 17:05:54 -0500

    Begin building login and signup routes

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+class UsersController < ApplicationController
+
+  get '/login' do
+    if !logged_in?
+      erb :'user/login'
+    else
+      # review this as I build
+      redirect '/user/show'
+    end
+  end
+
+  post '/login' do
+    user = User.find_by(:username => params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect 'user/show'
+    else
+      redirect 'user/signup'
+    end
+  end
+
+  get '/signup' do
+    erb :'user/signup'
+  end
+
+end
