0f4e05ac4760cc922c0d11cee2deea259445955b
jrog20 <jaimerogstad@gmail.com>
2020-11-02 17:01:53 -0500

    Add helper methods for login authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,9 @@ require './config/environment'
 
 class ApplicationController < Sinatra::Base
 
+  # register Sinatra::ActiveRecordExtension
+  # set :session_secret, "secret"
+
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
@@ -9,6 +12,18 @@ class ApplicationController < Sinatra::Base
 
   get "/" do
     erb :welcome
+  end
+
+  helpers do
+
+    def logged_in?
+      !!session[:user_id]
+    end
+  
+    def current_user
+      User.find(session[:user_id])
+    end
+
   end
 
 end
