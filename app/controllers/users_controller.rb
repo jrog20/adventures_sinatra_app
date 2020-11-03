cff1256f0e887c9a3350c546bbc96e207308424c
jrog20 <jaimerogstad@gmail.com>
2020-11-03 14:25:52 -0500

    Correct signup route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,12 +10,12 @@ class UsersController < ApplicationController
   end
 
   post '/login' do
-    user = User.find_by(:username => params[:username])
+    user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect 'user/show'
     else
-      redirect 'user/signup'
+      redirect '/signup'
     end
   end
 
