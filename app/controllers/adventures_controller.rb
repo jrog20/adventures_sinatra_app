e77b8ca3fc35d0adbeb07187cc289d0384fd6c93
jrog20 <jaimerogstad@gmail.com>
2020-11-05 19:51:44 -0500

    Finalize create functionality

--- app/controllers/adventures_controller.rb
+++ app/controllers/adventures_controller.rb
@@ -5,6 +5,16 @@ class AdventuresController < ApplicationController
     erb :'adventures/index'
   end
 
+  get '/adventures/new' do
+    erb :'adventures/new'
+  end
+
+  post '/adventures' do
+    adventure = Adventure.create(params)
+    adventure.user_id = current_user.id
+    redirect '/adventures/#{adventure.id}'
+  end
+
   get '/adventures/:id' do
     @adventure = Adventure.all.find_by(params[:id])
     erb :'adventures/show'
